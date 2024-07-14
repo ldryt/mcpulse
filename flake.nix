393cec501d8f14068370527dedfb3da49abc6f08
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-07-14 12:32:16 +0300

    nix flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          mcpulse = pkgs.buildGoModule {
+            pname = "mcpulse";
+            version = "0.4.1";
+
+            src = ./.;
+
+            vendorHash = "sha256-g+yaVIx4jxpAQ/+WrGKxhVeliYx7nLQe/zsGpxV4Fn4=";
+          };
+        }
+      );
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              delve
+            ];
+            # https://github.com/go-delve/delve/issues/3085
+            hardeningDisable = [ "fortify" ];
+          };
+        }
+      );
+    };
+}
